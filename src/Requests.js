d45347b2ecbd8823aaaf36a6f10f7df5913c3448
Fasina Farook <146752536+fasinafarook@users.noreply.github.com>
2024-06-10 20:31:49 +0530

    Add files via upload

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'aaef799cd32c37ecc33b2b3d579558d7'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${key}&language=en-US`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+export default requests
